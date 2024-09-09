acd5aa9ced8cc2809388a0a5a7cb565fad8c33f5
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-09-09 23:20:00 +0900

    type:[#feature]메인페이지 기본구성

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,12 +4,16 @@ import { createBrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 import {RouterObject} from './RouterList';
 import { RecoilRoot } from 'recoil';
+import MainPage from './pages/MainPage';
+// import './index.css';
+import './App.css';
 
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <RecoilRoot>
-            <RouterProvider router={RouterObject} />
+            {/* <RouterProvider router={RouterObject} /> */}
+            <MainPage/>
         </RecoilRoot>
     </StrictMode>,
 );
--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
-function MainPage(props) {
+function MainPage() {
     return (
-        <div>
-            
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="relative flex flex-col items-center mb-20">
+                <svg width="400" height="200" viewBox="0 0 400 200">
+                    <defs>
+                        <path
+                            id="curve"
+                            d="M 65,120
+                               A 200,200 0 0,1 340,120"
+                            fill="transparent"
+                        />
+                    </defs>
+                    <text width="400">
+                        <textPath
+                            href="#curve"
+                            startOffset="50%"
+                            textAnchor="middle"
+                            fontSize="36"
+                            fontWeight="bold"
+                            dominantBaseline="middle"
+                        >
+                            WORD BRIDGE
+                        </textPath>
+                    </text>
+                </svg>
+            </div>
+            <div className="flex flex-col space-y-4">
+                <button className="border-2 border-black rounded-full px-8 py-2 text-lg">게임방법</button>
+                <button className="border-2 border-black rounded-full px-8 py-2 text-lg">게임시작</button>
+            </div>
         </div>
     );
 }
