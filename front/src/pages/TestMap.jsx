4e583601cecac703f24c1563f0baa37d4626c4b1
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-09-11 19:26:38 +0900

    type:[#test] TestMap 파일명 변경

--- front/src/pages/TestMap.jsx
+++ front/src/pages/TestMap.jsx
@@ -4,7 +4,7 @@ import { useRecoilValue } from "recoil";
 import { mapAtom } from "../recoil/testAtom";
 "use client";
 
-export const Test = () => {
+export const TestMap = () => {
     const googleMapApi = import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY;
     const googleMapId = import.meta.env.VITE_APP_GOOGLE_MAPS_ID;
 
