1f8b94611442df6872a6b1720a5e8ab117a7fdeb
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-09-11 21:57:39 +0900

    type:[#test] Test 관련  파일 정정

--- front/src/RouterList.jsx
+++ front/src/RouterList.jsx
@@ -1,9 +1,10 @@
 // import { Layout } from "./layout/Layout.jsx";
 import MainPage from "./pages/MainPage.jsx";
 import { ChatPage } from "./pages/ChatPage.jsx";
-import { Test } from "./pages/Test.jsx";
+
 import { createBrowserRouter } from "react-router-dom";
 import { MapPage } from "./pages/MapPage.jsx";
+import Test from "./pages/Test.jsx";
 
 export const RouterList = () => [
     {
@@ -23,7 +24,7 @@ export const RouterList = () => [
         children: [
             {
                 path: "done",
-                element: <Test />,
+                element: <Test/>,
             },
         ],
     },
